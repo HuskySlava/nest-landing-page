66dc4f9221c2340b367cef08b1752f7d103693ad
Slava Mihaelov <huskyslava@gmail.com>
2026-02-24 22:46:49 +0200

    helmet package - secuirity headers

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ConfigService } from "@nestjs/config";
 import { ValidationPipe } from "@nestjs/common";
+import helmet from "helmet";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.use(helmet());
+
 	const configService = app.get(ConfigService);
 	const port = configService.get<number>("PORT") ?? 3000;
 	app.enableCors({
