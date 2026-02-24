eeba8830ea21b877fe23c1507767f8acd268d8d8
Slava Mihaelov <huskyslava@gmail.com>
2026-02-24 22:26:25 +0200

    Added submission-validation pipe to submission.module.ts

--- src/submission/submission.module.ts
+++ src/submission/submission.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SubmissionController } from "./submission.controller";
 import { SubmissionService } from "./submission.service";
+import { SubmissionValidationPipe } from "./submission-validation.pipe";
 
 @Module({
 	controllers: [SubmissionController],
-	providers: [SubmissionService],
+	providers: [SubmissionService, SubmissionValidationPipe],
 })
 export class SubmissionModule {}
