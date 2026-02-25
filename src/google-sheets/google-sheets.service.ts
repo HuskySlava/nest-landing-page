6d8c9c700157005995628612279c5cb009d83cf3
Slava Mihaelov <huskyslava@gmail.com>
2026-02-25 22:26:18 +0200

    Google sheets service - init with config

--- src/google-sheets/google-sheets.service.ts
+++ src/google-sheets/google-sheets.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { GoogleAuth } from "google-auth-library";
+import { google, sheets_v4 } from "googleapis";
 
 @Injectable()
-export class GoogleSheetsService {}
+export class GoogleSheetsService implements OnModuleInit {
+	private readonly logger = new Logger(GoogleSheetsService.name);
+	private sheets: sheets_v4.Sheets;
+
+	constructor(private readonly configService: ConfigService) {}
+
+	onModuleInit(){
+		const auth = new GoogleAuth({
+			keyFile: this.configService.get<string>("GOOGLE_KEY_FILE"),
+			scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+		});
+		this.sheets = google.sheets({ version: "v4", auth });
+		this.logger.log("Google Sheets Service initialized");
+	}
+}
