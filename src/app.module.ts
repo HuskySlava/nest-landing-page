42b6dee2440276ffc99237857a124affabd959de
Slava Mihaelov <huskyslava@gmail.com>
2026-02-26 08:23:02 +0200

    app module - bullmq BullModule import

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { formConfig } from "./config/form.config";
 import { APP_GUARD } from "@nestjs/core";
 import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
 import { ConfigService } from "@nestjs/config";
-import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
+import { GoogleSheetsModule } from "./google-sheets/google-sheets.module";
+import { BullModule } from "@nestjs/bullmq";
 
 @Module({
 	imports: [
@@ -24,6 +25,15 @@ import { GoogleSheetsModule } from './google-sheets/google-sheets.module';
 					limit: configService.get<number>("THROTTLE_LIMIT")!,
 				},
 			],
+			inject: [ConfigService],
+		}),
+		BullModule.forRootAsync({
+			useFactory: (configService: ConfigService) => ({
+				connection: {
+					host: configService.get<string>('REDIS_HOST'),
+					port: configService.get<number>('REDIS_PORT'),
+				},
+			}),
 			inject: [ConfigService],
 		}),
 		ServeStaticModule.forRoot({
